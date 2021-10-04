95ab7e5108cd1b3b561fd6c814cb1dc529ddd31f
jhon11390 <meljhon2@gmail.com>
2021-10-04 16:32:52 -0500

    Agrega links de datos de redes sociales en la seccion contactos

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,6 +4,30 @@ import {Row, Container, Col, Figure} from 'react-bootstrap'
 function Contact () {
   return (
     <Container id="Contact" className="mt-5" style={{backgroundColor: "black", color: "white"}}>
+      <style type="text/css">
+        {`
+          #facebook:hover {
+            transform: scale(1.3);
+          }
+
+          #twitter:hover {
+            transform: scale(1.3);
+          }
+
+          #instagram:hover {
+            transform: scale(1.3);
+          }
+
+          #github:hover {
+            transform: scale(1.3);
+          }
+
+          #linkedin:hover {
+            transform: scale(1.3);
+          }
+
+        `}
+        </style>
       <Row>
         <Col className="text-center m-5 p-5">
           <Figure>
@@ -33,68 +57,83 @@ function Contact () {
             <div className="text-center">
 
               <Figure style={{marginRight: "10px", cursor: "pointer"}}>
-                <Figure.Image
-                  width={40}
-                  height={40}
-                  alt="171x180"
-                  src="facebook.png"
-                  style={{filter: "invert(100%)"}}
-                />
-                <Figure.Caption>
-                  Facebook
-                </Figure.Caption>
+                <a href="https://www.facebook.com/jhon.ortegalopez.7" style={{textDecoration: "none"}} target="_blank" rel="noreferrer">
+                  <Figure.Image
+                    id="facebook"
+                    width={40}
+                    height={40}
+                    alt="171x180"
+                    src="facebook.png"
+                    style={{filter: "invert(100%)"}}
+                  />
+                  <Figure.Caption>
+                    Facebook
+                  </Figure.Caption>
+                </a>
               </Figure>
 
               <Figure style={{marginRight: "10px", cursor: "pointer"}}>
-                <Figure.Image
-                  width={40}
-                  height={40}
-                  alt="171x180"
-                  src="twitter.png"
-                  style={{filter: "invert(100%)"}}
-                />
-                <Figure.Caption>
-                  Twitter
-                </Figure.Caption>
+                <a href="https://twitter.com/Jhon11390" style={{textDecoration: "none"}} target="_blank" rel="noreferrer">
+                  <Figure.Image
+                    id="twitter"
+                    width={40}
+                    height={40}
+                    alt="171x180"
+                    src="twitter.png"
+                    style={{filter: "invert(100%)"}}
+                  />
+                  <Figure.Caption>
+                    Twitter
+                  </Figure.Caption>
+                </a>
               </Figure>
 
               <Figure style={{marginRight: "10px", cursor: "pointer"}}>
-                <Figure.Image
-                  width={40}
-                  height={40}
-                  alt="171x180"
-                  src="instagram.png"
-                  style={{filter: "invert(100%)"}}
-                />
-                <Figure.Caption>
-                  Instagram
-                </Figure.Caption>
+                <a href="https://www.instagram.com/meljhon2/" style={{textDecoration: "none"}} target="_blank" rel="noreferrer">
+                  <Figure.Image
+                    id="instagram"
+                    width={40}
+                    height={40}
+                    alt="171x180"
+                    src="instagram.png"
+                    style={{filter: "invert(100%)"}}
+                  />
+                  <Figure.Caption>
+                    Instagram
+                  </Figure.Caption>
+                </a>
               </Figure>
 
               <Figure style={{marginRight: "10px", cursor: "pointer"}}>
-                <Figure.Image
-                  width={40}
-                  height={40}
-                  alt="171x180"
-                  src="github.png"
-                  style={{filter: "invert(100%)"}}
-                />
-                <Figure.Caption>
-                  Github
-                </Figure.Caption>
+                <a href="https://github.com/jhon11390" style={{textDecoration: "none"}} target="_blank" rel="noreferrer">
+                  <Figure.Image
+                    id="github"
+                    width={40}
+                    height={40}
+                    alt="171x180"
+                    src="github.png"
+                    style={{filter: "invert(100%)"}}
+                  />
+                  <Figure.Caption>
+                    Github
+                  </Figure.Caption>
+                </a>
               </Figure>
 
               <Figure style={{cursor: "pointer"}}>
-                <Figure.Image
-                  width={40}
-                  height={40}
-                  alt="171x180"
-                  src="linkedin.png"
-                  style={{filter: "invert(100%)"}}
-                />
-                <Figure.Caption>
-                  Linkedin
-                </Figure.Caption>
+                <a href="https://www.linkedin.com/in/jhon-ortega-89557a194/" style={{textDecoration: "none"}} target="_blank" rel="noreferrer">
+                  <Figure.Image
+                    id="linkedin"
+                    width={40}
+                    height={40}
+                    alt="171x180"
+                    src="linkedin.png"
+                    style={{filter: "invert(100%)"}}
+                  />
+                  <Figure.Caption>
+                    Linkedin
+                  </Figure.Caption>
+                </a>
               </Figure>
             </div>
           </div>
